e96c31db94fb38e9775f77855e540915ce879c4b
Muhammed Shahed Chowdhury <rsashahed123@gmail.com>
2020-09-04 17:08:58 +0200

    Already running a demo greeting endpoints with hello world demo. Now implementing the logical calculation of the endpoint.

---
+++ src/main/java/com/example/endpoinds/EndpoindsApplication.java
@@ -0,0 +1,13 @@
+package com.example.endpoinds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndpoindsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndpoindsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/endpoinds/EndpoindsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.endpoinds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndpoindsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
