a37cb14a3a93452e5f3b42689f61fee3020d7d6b
Muhammed Shahed Chowdhury <rsashahed123@gmail.com>
2020-09-04 18:46:05 +0200

    Done testing.

--- src/main/java/com/example/endpoinds/greetingControler.java
+++ src/main/java/com/example/endpoinds/greetingControler.java
@@ -13,57 +13,130 @@ public class greetingControler {
 
     Logger logger = LoggerFactory.getLogger(greetingControler.class);
 
+    public static String scan(String input){
+        String result = null;
+        String cleanInput = input.replaceAll("[^-0-9]", "");
+        if(!cleanInput.isEmpty()){
+            result  = cleanInput;
+        }
+        return result;
+    }
+
+    // Converting kelvin into celsius
     @GetMapping("/conversions/ktoc/{id}")
-    public String getFoosBySimplePathWithPathVariable(
+    public String celsius(
             @PathVariable("id") String id){
 
-        String cleanInput = id.replaceAll("[^0-9]", "");
-        if(cleanInput.isEmpty()){
-            return "{" +
-                    "   \"status\":400," +
-                    "   \"description\":\"Invalid data supplied\"" +
+        String cleanInput = scan(id);
+
+        if(cleanInput == null){
+            String errorMsg =  "{\n" +
+                    "   \"status\":400,\n" +
+                    "   \"description\":\"Invalid data supplied\"\n" +
                     "}";
+            logger.info("Convert Failed!\n" + errorMsg);
+            return errorMsg;
         }
 
-        String result = "{\n";
-        result += " \"Kelvin\" : " + cleanInput + ",";
-
-        double Celsius = Integer.parseInt(cleanInput) - 273.15;
-
+        double convert = Integer.parseInt(cleanInput) - 273.15;
         DecimalFormat df = new DecimalFormat("####0.00");
 
-        result += " \"Celsius\" : " + df.format(Celsius);
-        result += "}\n";
-        logger.info("An INFO Message");
+        String result = "{\n";
+        result += " \"kelvin\" : " + cleanInput + ",\n";
+        result += " \"celsius\" : " + df.format(convert) + "\n";
+        result += "}";
+
+        logger.info("Convert Successfully!\n" + result);
+
         return result;
     }
 
-
-
+    // Converting celsius into kelvin
     @GetMapping("/conversions/ctok/{id}")
-    public String e(
+    public String kelvin(
             @PathVariable("id") String id){
 
-        String cleanInput = id.replaceAll("[^0-9]", "");
-        if(cleanInput.isEmpty()){
-            return "{" +
-                    "   \"status\":400," +
-                    "   \"description\":\"Invalid data supplied\"" +
+        String cleanInput = scan(id);
+
+        if(cleanInput == null){
+            String errorMsg =  "{\n" +
+                    "   \"status\":400,\n" +
+                    "   \"description\":\"Invalid data supplied\"\n" +
                     "}";
+            logger.info("Convert Failed!\n" + errorMsg);
+            return errorMsg;
         }
 
-        String result = "{\n";
-        result += " \"Kelvin\" : " + cleanInput + ",";
-
-        double Celsius = Integer.parseInt(cleanInput) - 273.15;
-
+        double convert = Integer.parseInt(cleanInput) + 273.15;
         DecimalFormat df = new DecimalFormat("####0.00");
 
-        result += " \"Celsius\" : " + df.format(Celsius);
-        result += "}\n";
-        logger.info("An INFO Message");
+        String result = "{\n";
+        result += " \"celsius\" : " + cleanInput + ",\n";
+        result += " \"kelvin\" : " + df.format(convert) + "\n";
+        result += "}";
+
+        logger.info("Convert Successfully!\n" + result);
+
         return result;
     }
 
+
+    //Converting miles to kilometers.
+    @GetMapping("/conversions/mtok/{id}")
+    public String kilometers(
+            @PathVariable("id") String id){
+
+        String cleanInput = scan(id);
+
+        if(cleanInput == null){
+            String errorMsg =  "{\n" +
+                    "   \"status\":400,\n" +
+                    "   \"description\":\"Invalid data supplied\"\n" +
+                    "}";
+            logger.info("Convert Failed!\n" + errorMsg);
+            return errorMsg;
+        }
+
+        double convert = Integer.parseInt(cleanInput) * 1.60934;
+        DecimalFormat df = new DecimalFormat("####0.00");
+
+        String result = "{\n";
+        result += " \"miles\" : " + cleanInput + ",\n";
+        result += " \"kilometers\" : " + df.format(convert) + "\n";
+        result += "}";
+
+        logger.info("Convert Successfully!\n" + result);
+
+        return result;
+    }
+
+    //Converting kilometers to miles.
+    @GetMapping("/conversions/ktom/{id}")
+    public String miles(
+            @PathVariable("id") String id){
+
+        String cleanInput = scan(id);
+
+        if(cleanInput == null){
+            String errorMsg =  "{\n" +
+                    "   \"status\":400,\n" +
+                    "   \"description\":\"Invalid data supplied\"\n" +
+                    "}";
+            logger.info("Convert Failed!\n" + errorMsg);
+            return errorMsg;
+        }
+
+        double convert = Integer.parseInt(cleanInput) / 1.60934;
+        DecimalFormat df = new DecimalFormat("####0.00");
+
+        String result = "{\n";
+        result += " \"kilometers\" : " + cleanInput + ",\n";
+        result += " \"miles\" : " + df.format(convert) + "\n";
+        result += "}";
+
+        logger.info("Convert Successfully!\n" + result);
+
+        return result;
+    }
 
 }
