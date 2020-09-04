e53a782fff4402222d05fc1b43c16e2fd625fbce
Muhammed Shahed Chowdhury <rsashahed123@gmail.com>
2020-09-04 18:06:14 +0200

    Implementing log and returns.

--- src/main/java/com/example/endpoinds/greetingControler.java
+++ src/main/java/com/example/endpoinds/greetingControler.java
@@ -1,20 +1,68 @@
 package com.example.endpoinds;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.text.DecimalFormat;
 
 @RestController
 public class greetingControler {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    Logger logger = LoggerFactory.getLogger(greetingControler.class);
 
-    @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name){
-        return new Greeting(counter.incrementAndGet(),String.format(template,name));
+    @GetMapping("/conversions/ktoc/{id}")
+    public String getFoosBySimplePathWithPathVariable(
+            @PathVariable("id") String id){
+
+        String cleanInput = id.replaceAll("[^0-9]", "");
+        if(cleanInput.isEmpty()){
+            return "{" +
+                    "   \"status\":400," +
+                    "   \"description\":\"Invalid data supplied\"" +
+                    "}";
+        }
+
+        String result = "{\n";
+        result += " \"Kelvin\" : " + cleanInput + ",";
+
+        double Celsius = Integer.parseInt(cleanInput) - 273.15;
+
+        DecimalFormat df = new DecimalFormat("####0.00");
+
+        result += " \"Celsius\" : " + df.format(Celsius);
+        result += "}\n";
+        logger.info("An INFO Message");
+        return result;
+    }
+
+
+
+    @GetMapping("/conversions/ctok/{id}")
+    public String e(
+            @PathVariable("id") String id){
+
+        String cleanInput = id.replaceAll("[^0-9]", "");
+        if(cleanInput.isEmpty()){
+            return "{" +
+                    "   \"status\":400," +
+                    "   \"description\":\"Invalid data supplied\"" +
+                    "}";
+        }
+
+        String result = "{\n";
+        result += " \"Kelvin\" : " + cleanInput + ",";
+
+        double Celsius = Integer.parseInt(cleanInput) - 273.15;
+
+        DecimalFormat df = new DecimalFormat("####0.00");
+
+        result += " \"Celsius\" : " + df.format(Celsius);
+        result += "}\n";
+        logger.info("An INFO Message");
+        return result;
     }
 
 
